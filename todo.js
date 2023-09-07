085ed623d05dada97300f11aa1bc515bc01557b8
josephneil <118806784+josephneil@users.noreply.github.com>
2023-09-07 19:34:40 +0800

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,15 @@
+function addListItem() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("input").value;
+  var t = document.createTextNode(inputValue);
+
+  li.appendChild(t);
+
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("tasks").appendChild(li);
+  }
+
+  document.getElementById("input").value = "";
+}
